1aff5bdd031dd7e4d00bf1a09b24173ed721a4e0
Modenero <support@modenero.com>
2019-12-20 06:05:44 -0500

    Some refactoring.

--- src/components/Sidebar/MainPanel.vue
+++ src/components/Sidebar/MainPanel.vue
@@ -47,6 +47,11 @@
             <ul class="nav nav-aside">
                 <div class="divider-text">Workspaces</div>
 
+                <!-- Workspace Search -->
+                <div class="search-form">
+                    <input id="search-workspace" type="search" class="form-control" placeholder="Search for a workspace">
+                </div>
+
                 <li class="nav-item"><a href="/defi360" class="nav-link"><i data-feather="globe"></i> <span>DeFi Admin <small class="ml-1">[ Management ]</small></span></a></li>
 
                 <li class="nav-item"><a href="/defi360" class="nav-link"><i data-feather="umbrella"></i> <span>DeFi Project <small class="ml-1">[ Collaborate ]</small></span></a></li>
@@ -77,10 +82,55 @@ export default {
         }
     },
     methods: {
-        //
+        initTypeAhead () {
+            const substringMatcher = function(strs) {
+                return function findMatches(q, cb) {
+                    var matches, substrRegex;
+
+                    // an array that will be populated with substring matches
+                    matches = [];
+
+                    // regex used to determine if a string contains the substring `q`
+                    substrRegex = new RegExp(q, 'i');
+
+                    // iterate through the pool of strings and for any string that
+                    // contains the substring `q`, add it to the `matches` array
+                    $.each(strs, function(i, str) {
+                        if (substrRegex.test(str)) {
+                            matches.push(str);
+                        }
+                    });
+
+                    cb(matches);
+                };
+            };
+
+            var states = ['Alabama', 'Alaska', 'Arizona', 'Arkansas', 'California',
+                'Colorado', 'Connecticut', 'Delaware', 'Florida', 'Georgia', 'Hawaii',
+                'Idaho', 'Illinois', 'Indiana', 'Iowa', 'Kansas', 'Kentucky', 'Louisiana',
+                'Maine', 'Maryland', 'Massachusetts', 'Michigan', 'Minnesota',
+                'Mississippi', 'Missouri', 'Montana', 'Nebraska', 'Nevada', 'New Hampshire',
+                'New Jersey', 'New Mexico', 'New York', 'North Carolina', 'North Dakota',
+                'Ohio', 'Oklahoma', 'Oregon', 'Pennsylvania', 'Rhode Island',
+                'South Carolina', 'South Dakota', 'Tennessee', 'Texas', 'Utah', 'Vermont',
+                'Virginia', 'Washington', 'West Virginia', 'Wisconsin', 'Wyoming'
+            ];
+
+            $('#search-workspace').typeahead({
+                hint: true,
+                highlight: true,
+                minLength: 1
+            }, {
+                name: 'states',
+                source: substringMatcher(states)
+            });
+        },
     },
     mounted: function () {
         console.log('Sidebar is mounted!')
+
+        /* Initialize type ahead. */
+        this.initTypeAhead()
     },
 }
 </script>
